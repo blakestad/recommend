846aa8d9813fcd159fe95e8162097cc0c0b72240
Cliff Blakestad <data.blakestad@gmail.com>
2023-12-31 21:07:23 -0600

    Removed small sample version

--- recommender/initialize_model.py
+++ recommender/initialize_model.py
@@ -3,16 +3,13 @@ import joblib
 from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def initialize(small_version=False):
+def initialize():
     """
     Initializes the paper-titles dataframe and Tfidf-Vectorizers used for the recommend module.
 
-    Can run a small version for debugging which loads faster into memory.    
-
     Parameters
     ----------
-    small_version : boolean
-        Determines if the debug versions of the model is loaded (Default value = False).
+
 
     Returns
     -------
@@ -33,24 +30,18 @@ def initialize(small_version=False):
         A matrix of the tfidf-vectorized abstract for each math paper used from the arXiv.
     """
 
-    if small_version==True:
-        fp = 'arxiv_sample/sample_'
-        print('Small debug dataset in use.')
-    else:
-        fp = 'arxiv_data/tfidf_'
-        print('Full dataset in use.')
 
     # Load data
-    math_papers = pd.read_pickle('recommender/' + fp + 'math_arxiv_titles.pkl')
+    math_papers = pd.read_pickle('recommender/arxiv_data/tfidf_math_arxiv_titles.pkl')
 
     # Load model data
-    tfidf_title_vectorizer = joblib.load('recommender/' + fp + 'title_vectorizer.pkl')
-    tfidf_abstract_vectorizer = joblib.load('recommender/' + fp + 'abstract_vectorizer.pkl')
+    tfidf_title_vectorizer = joblib.load('recommender/arxiv_data/tfidf_title_vectorizer.pkl')
+    tfidf_abstract_vectorizer = joblib.load('recommender/arxiv_data/tfidf_abstract_vectorizer.pkl')
 
-    tfidf_title_matrix = joblib.load('recommender/' + fp + 'title_matrix.pkl')
-    tfidf_abstract_matrix = joblib.load('recommender/' + fp + 'abstract_matrix.pkl')
+    tfidf_title_matrix = joblib.load('recommender/arxiv_data/tfidf_title_matrix.pkl')
+    tfidf_abstract_matrix = joblib.load('recommender/arxiv_data/tfidf_abstract_matrix.pkl')
 
     return math_papers, tfidf_title_vectorizer, tfidf_abstract_vectorizer, tfidf_title_matrix, tfidf_abstract_matrix
 
 
-math_papers, tfidf_title_vectorizer, tfidf_abstract_vectorizer, tfidf_title_matrix, tfidf_abstract_matrix = initialize(small_version=True)
+math_papers, tfidf_title_vectorizer, tfidf_abstract_vectorizer, tfidf_title_matrix, tfidf_abstract_matrix = initialize()
